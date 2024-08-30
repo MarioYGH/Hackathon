e5def39b6a549224a0b0f98c325b136611c30e11
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2024-08-30 16:30:01 -0600

    Update analisistopo.py

--- Topografia/analisistopo.py
+++ Topografia/analisistopo.py
@@ -1,5 +1,6 @@
 import rasterio
 import numpy as np
+import matplotlib.pyplot as plt 
 
 def analizar_topografia(ruta_archivo):
     """
@@ -25,9 +26,36 @@ def analizar_topografia(ruta_archivo):
 
     return gradiente_x, gradiente_y, pendiente
 
+def graficar_gradientes(gradiente_x, gradiente_y):
+    """
+    Grafica los gradientes en las direcciones X e Y de un análisis topográfico.
+
+    Args:
+        gradiente_x (np.ndarray): Gradiente en la dirección X.
+        gradiente_y (np.ndarray): Gradiente en la dirección Y.
+    """
+    # Crear una figura con dos subgráficas
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Graficar el gradiente en X
+    cax1 = axs[0].imshow(gradiente_x, cmap='viridis')
+    axs[0].set_title('Gradiente en X')
+    fig.colorbar(cax1, ax=axs[0])
+
+    # Graficar el gradiente en Y
+    cax2 = axs[1].imshow(gradiente_y, cmap='viridis')
+    axs[1].set_title('Gradiente en Y')
+    fig.colorbar(cax2, ax=axs[1])
+
+    plt.tight_layout()
+    plt.show()
+    
+
 # Ejemplo de uso:
-ruta_archivo = 'path_to_your_elevation_data.tif'
+ruta_archivo = 'output_NASADEM.tif'
 gradiente_x, gradiente_y, pendiente = analizar_topografia(ruta_archivo)
+
+graficar_gradientes(gradiente_x, gradiente_y)
 
 # Imprimir los resultados
 print("Gradiente X:\n", gradiente_x)
